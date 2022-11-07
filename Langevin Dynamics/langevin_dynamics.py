fixed numerical issues with code, converges to proper solution

Langevin Dynamics/langevin_dynamics.py
@@ -1,12 +1,14 @@
 from numba import njit
 import numpy as np
 from numba import jit
+from numpy.random import default_rng
 from small_world_network import smallWorldNetwork
+from tqdm import tqdm
 
 
 class langevinDynamics():
     def __init__(self, SWN: smallWorldNetwork, n: int,
-                 dt: float, T: float):
+                 dt: float, T: float, b_n: int):
         """
 
         :param SWN: generated small world network object
@@ -14,25 +16,36 @@ class langevinDynamics():
         :param nb: number of desired burn in steps (will be chosen to be >= sqrt(N)
         :param dt: evolution time step
         :param T: temperature
+        :param b_n: initial burn in period
         """
         self.SWN = SWN
         self.N = SWN.N
         self.n = n
         self.dt = dt
         self.T = T
+        self.b_n = b_n
+        self.rng = default_rng()
+        self.sigma = np.sqrt(2 * self.T * self.dt)
         self.samples = np.zeros((self.N, n))
+        self.passage_time = int(self.N / self.dt)
         self.current_state = SWN.spins # randomly initialized spin vector from SWN
 
     def state_update(self, update: np.array):
         self.current_state = update
-    def dynamics(self) -> np.array:
-        system_fluc = int(np.sqrt(self.N))
-        sigma = np.sqrt(2*self.T/self.dt)
 
-        for i in range(self.n):
-            for ii in range(system_fluc):
-                ds = self.SWN.force()
-                s_i = self.current_state.copy() - ds*self.dt + sigma*np.random.randn(self.N)*self.dt
+    def burn_in(self):
+        for i in tqdm(range(self.b_n), desc="Burning in the system"):
+            ds = np.add(self.SWN.force(self.current_state)*self.dt, self.rng.normal(size=self.N)*np.sqrt(2*self.T*self.dt))
+            s_i = np.add(self.current_state, ds)
+            self.state_update(s_i)
+    def dynamics(self):
+        self.burn_in()
+
+        for i in tqdm(range(self.n), desc="Sampling"):
+            for ii in range(self.passage_time):
+                ds = np.add(self.SWN.force(self.current_state) * self.dt,
+                            self.rng.normal(size=self.N) * np.sqrt(2 * self.T * self.dt))
+                s_i = np.add(self.current_state, ds)
                 self.state_update(s_i)
 
             self.samples[:, i] = self.current_state

Langevin Dynamics/main.py
@@ -5,17 +5,29 @@ from small_world_network import smallWorldNetwork
 
 # network parameters
 N = 100
-c = 0.1
+c = (1/0.6)
 J = 1
-J0 = 1
+J0 = -0.5
 
 # simulation parameters
-T = 0.3
-n = 10
+T = 0.4
+n = int(np.sqrt(N))
 dt = 0.01
+b_n = 1000
 
 SWN = smallWorldNetwork(N, c, J, J0)
-langevinDynamics = langevinDynamics(SWN, n, dt, T)
+langevinDynamics = langevinDynamics(SWN, n, dt, T, b_n)
 langevinDynamics.dynamics()
+s = langevinDynamics.samples
+x = np.cos(s)
+y = np.sin(s)
+m_x = x.mean()
+m_y = y.mean()
+m = np.sqrt(m_x**2 + m_y**2)
+q_xx = np.mean(np.power(x.mean(axis=1),2))
+q_yy = np.mean(np.power(y.mean(axis=1),2))
+q = q_xx + q_yy
+print('-'*20)
+print(f'm_x: {m_x} \n m_y: {m_y} \n m: {m} \n q_xx: {q_xx} \n q_yy: {q_yy} \n q: {q}')
 print(langevinDynamics.current_state)
 print(langevinDynamics.samples)